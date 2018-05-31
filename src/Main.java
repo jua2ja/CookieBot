30299b46d47a033d8a6ac4453a15008187cf6b3a
thebeninator1 <thebeninator1@gmail.com>
2018-05-31 13:20:45 -0400

    cut the bits from main

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,6 @@ import org.opencv.core.Point;
 public class Main {
 
 	public static void main(String[] args) {
-		long cookies = 0;
-		int[] buildingCounts = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		long[] buildingCosts = {15L, 100L, 500L, 3000L, 10000L, 40000L, 200000L, 1666666L, 123456789L, 3999999999L};
-		double[] buildingProduction = {0.1, 1, 8, 47, 260};
 		boolean running = false;
 		CookieGUI gui = new CookieGUI();
 		FindGameElements find = new FindGameElements(); //uses the default constructor that takes a picture of the current screen
